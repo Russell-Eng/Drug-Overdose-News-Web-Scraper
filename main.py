342dfa98036fcc632cefe79e37b2735181fa99e2
Russell-Eng <rseng6@uw.edu>
2023-04-28 14:09:11 -0700

    go into each article

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 url = "https://www.dea.gov/what-we-do/news/press-releases?f%5B0%5D=press_year%3A2020&page=0"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
-print(soup)
+# print(soup)
 
 # Find all the press release titles and dates
 titles = soup.find_all("h3", class_="teaser__heading")
@@ -14,11 +14,18 @@ links = []
 for title in titles:
     a_tag = title.find("a")
     if a_tag:
-        links.append(a_tag)
+        links.append("https://www.dea.gov/" + str(a_tag["href"]))
+
+for link in links:
+    article_url = link
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    print(soup)
+
 
 # Print the title and date of each press release
-for i in range(len(titles)):
-    title = titles[i].text.strip()
-    date = dates[i].text.strip()
-    link = links[i]["href"]
-    print(f"{date} - {title} - https://www.dea.gov/{link}")
+# for i in range(len(titles)):
+#     title = titles[i].text.strip()
+#     date = dates[i].text.strip()
+#     # link = links[i]["href"]
+#     print(f"{date} - {title} - {links[i]}")
