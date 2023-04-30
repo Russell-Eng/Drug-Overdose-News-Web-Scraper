496622245360d2f0e4f67d54f0362512413bb69b
Russell-Eng <rseng6@uw.edu>
2023-04-30 01:34:06 -0700

    scrape news into dataframe. Date column not working

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
-url = "https://www.dea.gov/what-we-do/news/press-releases?f%5B0%5D=press_year%3A2020&page=0"
+url = "https://www.dea.gov/what-we-do/news/press-releases?page=0"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 # print(soup)
@@ -16,14 +17,39 @@ for title in titles:
     if a_tag:
         links.append("https://www.dea.gov/" + str(a_tag["href"]))
 
-press_titles = []
+press_summary = []
+data = {
+    "Title": [],
+    "Date": [],
+    "Summary": [],
+    "Location": []
+}
 for link in links:
     article_url = link
     response = requests.get(article_url)
     soup = BeautifulSoup(response.content, "html.parser")
-    press_titles.append(soup.find_all("h2", class_="press__title"))
 
-print(press_titles)
+    date = soup.find("div", class_="press__date").text.strip()
+    title = soup.find("h2", class_="press__title").text.strip()
+    paragraph_div = soup.find("div", class_="wysiwyg")
+    first_paragraph = paragraph_div.find("p").text.strip()
+    index = len(first_paragraph)
+    for char in ["-", "—", "–", "–"]:
+        char_index = first_paragraph.find(char)
+        if char_index != -1 and char_index < index:
+            index = char_index
+            break
+    location = first_paragraph[:index].strip()
+
+    data["Title"].append(title)
+    data["Date"].append(date)
+    data["Summary"].append(first_paragraph)
+    data["Location"].append(location)
+    # press_summary.append(f"{date} - {title} - {first_paragraph}")
+
+# print(press_summary)
+df = pd.DataFrame(data)
+print(df["Location"])
 
 
 # Print the title and date of each press release
