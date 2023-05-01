90ad4d391b89aef4300916d6f5b4779871d0468c
Russell-Eng <rseng6@uw.edu>
2023-05-01 01:48:04 -0700

    no encoding

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-pages = 5
+pages = 1
 links = []
 for i in range(pages):
     url = f"https://www.dea.gov/what-we-do/news/press-releases?page={i}"
@@ -29,8 +29,8 @@ for link in links:
     response = requests.get(article_url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    date = soup.find("div", class_="press__date").text.strip().encode("utf-8")
-    title = soup.find("h2", class_="press__title").text.strip().encode("utf-8")
+    date = soup.find("div", class_="press__date").text.strip()
+    title = soup.find("h2", class_="press__title").text.strip()
     paragraph_div = soup.find("div", class_="wysiwyg")
     first_paragraph = paragraph_div.find("p").text.strip()
     index = len(first_paragraph)
@@ -39,17 +39,27 @@ for link in links:
         if char_index != -1 and char_index < index:
             index = char_index
             break
-    location = first_paragraph[:index].strip().encode("utf-8")
+    location = first_paragraph[:index].strip()
+
+    # print(date)
+    #
+    # # Check if string starts with a quote
+    # if title.startswith('"'):
+    #     # Check if string ends with a quote
+    #     if title.endswith('"'):
+    #         # Remove quotes using string slicing
+    #         title = title[1:-1]
 
     data["Title"].append(title)
     data["Date"].append(date)
-    data["Summary"].append(first_paragraph.encode("utf-8"))
+    data["Summary"].append(first_paragraph)
     data["Location"].append(location)
     data["Link"].append(link)
 
 df = pd.DataFrame(data)
 
-df.to_csv("news.csv", index=False)
+
+df.to_csv("news.csv", index=False, quoting=0)
 
 
 
